4b200b24f1181594c32ce8445e96c929b9689c4a
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2020-12-13 22:01:51 -0500

    Optional Of, OfNullable and Empty example

---
+++ java8_features/Optional/src/com/seth/java/optional/OptionalOfEmptyNullableExample.java
@@ -0,0 +1,29 @@
+package com.seth.java.optional;
+
+import java.util.Optional;
+
+public class OptionalOfEmptyNullableExample {
+
+	public static Optional<String> ofNullable(){
+		Optional<String> stringOptional = Optional.ofNullable("Hello");
+		//Optional<String> stringOptional = Optional.ofNullable(null);
+		return stringOptional;
+	}
+	
+	public static Optional<String> of(){
+		Optional<String> stringOptional = Optional.of("Hello");
+		//Optional<String> stringOptional = Optional.of(null);
+		return stringOptional;
+	}
+	
+	public static Optional<String> empty(){
+		return Optional.empty();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("ofNullable - " + ofNullable());
+		System.out.println("of - " + of());
+		System.out.println("empty - " + empty());
+	}
+
+}
